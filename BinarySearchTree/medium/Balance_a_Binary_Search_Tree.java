8f71755b0491f24f064d6d86a3ef566bcc19667e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-06-26 08:10:32 +0530

    add the question solution on leetcode

--- BinarySearchTree/medium/Balance_a_Binary_Search_Tree.java
+++ BinarySearchTree/medium/Balance_a_Binary_Search_Tree.java
@@ -1,3 +1,31 @@
 //https://leetcode.com/problems/balance-a-binary-search-tree/description/?envType=daily-question&envId=2024-06-26
 
 //1382. Balance a Binary Search Tree
+import java.util.*;
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+
+    private void dfs(TreeNode node) {
+        if (node == null)
+            return;
+
+        dfs(node.left);
+        list.add(node.val);
+        dfs(node.right);
+    }
+
+    private TreeNode constructTree(int l, int r) {
+        if (l > r)
+            return null;
+
+        var m = l + (r - l) / 2;
+
+        return new TreeNode(list.get(m), constructTree(l, m - 1), constructTree(m + 1, r));
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+        dfs(root);
+        return constructTree(0, list.size() - 1);
+    }
+}
