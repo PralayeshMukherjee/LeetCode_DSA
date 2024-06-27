2baca3fa165875ec06ba631474e24705b1720389
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-06-27 18:30:35 +0530

    add the solution of the question

--- 2D_Array/easy/Find_Center_of_Star_Graph.java
+++ 2D_Array/easy/Find_Center_of_Star_Graph.java
@@ -1,3 +1,13 @@
 //https://leetcode.com/problems/find-center-of-star-graph/description/?envType=daily-question&envId=2024-06-27
 
 // 1791. Find Center of Star Graph
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        if((edges[0][0]==edges[1][1]) || edges[0][0]==edges[1][0]){
+            return edges[0][0];
+        }else{
+            return edges[0][1];
+        }
+    }
+}
