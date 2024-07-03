9d516ee8e4123a2e2c70071bb153774a60fda0db
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-03 09:01:02 +0530

    add the package for the arrays class

--- array/easy/Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.java
+++ array/easy/Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.java
@@ -2,7 +2,7 @@
 
 // 1509. Minimum Difference Between Largest and Smallest Value in Three Moves
 
-
+import java.util.*;
 class Solution {
     public int minDifference(int[] nums) {
         if(nums.length <= 4) {
