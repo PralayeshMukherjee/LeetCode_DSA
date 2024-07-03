7f8aacd8db1559aada8654b96fe2a26e8476de7a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-03 08:56:36 +0530

    add the number and name of the question

--- array/easy/Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.java
+++ array/easy/Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.java
@@ -1,2 +1,3 @@
 //https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/description/?envType=daily-question&envId=2024-07-03
 
+// 1509. Minimum Difference Between Largest and Smallest Value in Three Moves
