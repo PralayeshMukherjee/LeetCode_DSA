20c532c4aad94901cc8a68fd98f5856f8da1f7a9
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-06 10:01:42 +0530

    add the solution of the pass the pillow problem question

--- Math/easy/Pass_the_Pillow.java
+++ Math/easy/Pass_the_Pillow.java
@@ -1,3 +1,17 @@
 //https://leetcode.com/problems/pass-the-pillow/?envType=daily-question&envId=2024-07-06
 
 //2582. Pass the Pillow
+
+class Solution {
+    public int passThePillow(int n, int time) {
+        int direction = -1,count = 0,st=1;
+        while(count!=time){
+            if(st==n || st==1){
+                direction *= -1;
+            }
+            st += direction;
+            count++;
+        }
+        return st;
+    }
+}
