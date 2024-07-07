36817c4df851f3a09a54ac44a18e0bcafd61991e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-07 11:47:15 +0530

    add the solution of the question

--- Math/easy/Water_Bottles.java
+++ Math/easy/Water_Bottles.java
@@ -1,3 +1,14 @@
 // https://leetcode.com/problems/water-bottles/description/?envType=daily-question&envId=2024-07-07
 
 // 1518. Water Bottles
+
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        while(numBottles>=numExchange){
+            total += numBottles/numExchange;
+            numBottles = (numBottles/numExchange) + (numBottles%numExchange);
+        }
+        return total;
+    }
+}
