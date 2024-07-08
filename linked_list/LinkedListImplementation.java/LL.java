b363d834e1a8d03de129bd9ab927ead05f34a375
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-08 20:57:23 +0530

    add a method for adding a new node in the first of the linked list

--- linked_list/LinkedListImplementation.java/LL.java
+++ linked_list/LinkedListImplementation.java/LL.java
@@ -1,13 +1,21 @@
-
-class InnerLL {
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
 public class LL {
+    Node head;
+    class Node {
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void addFirst(int data){
+        if(head==null){
+            return;
+        }
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
     public static void main(String[] args) {
         
     }
