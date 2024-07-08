d5122f3fa34566714043ff82943f16b363a88cbf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-08 20:51:58 +0530

    add the blur print of the linked list class

--- linked_list/LinkedListImplementation.java/LL.java
+++ linked_list/LinkedListImplementation.java/LL.java
@@ -1,3 +1,12 @@
+
+class InnerLL {
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
 public class LL {
     public static void main(String[] args) {
         
