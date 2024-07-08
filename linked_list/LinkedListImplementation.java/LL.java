1c7c64bd9a601096a8f200eeee1c6d4696167c73
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-08 21:03:02 +0530

    add the method for the length of the linked list

--- linked_list/LinkedListImplementation.java/LL.java
+++ linked_list/LinkedListImplementation.java/LL.java
@@ -20,11 +20,20 @@ public class LL {
         if(head==null) return;
         Node newNode = new Node(data);
         Node currNode = head;
-        while(currNode.next==null){
+        while(currNode.next!=null){
             currNode = currNode.next;
         }
         currNode.next = newNode;
         newNode.next = null;
+    }
+    public void size(){
+        Node curNode = head;
+        int count = 0;
+        while (curNode!=null) {
+            count++;
+            curNode = curNode.next;
+        }
+        System.out.println("the size of the array is:=> "+count);
     }
     public static void main(String[] args) {
         
