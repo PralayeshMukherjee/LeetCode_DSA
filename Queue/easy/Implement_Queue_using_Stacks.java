3dc3115eacdf5e8874d0c52aaf811ade1cde28ce
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-09 20:32:00 +0530

    add the solution of the question which is basically queue implementation using arraylist

--- Queue/easy/Implement_Queue_using_Stacks.java
+++ Queue/easy/Implement_Queue_using_Stacks.java
@@ -1,1 +1,36 @@
+import java.util.ArrayList;
+class MyQueue {
+    public ArrayList<Integer> arr;
+    public int front;
+    public MyQueue() {
+        arr = new ArrayList<>();
+        front = 0;
+    }
+    
+    public void push(int x) {
+        arr.add(x);
+    }
+    
+    public int pop() {
+        int element = arr.get(front);
+        front++;
+        return element;
+    }
+    
+    public int peek() {
+        return arr.get(front);
+    }
+    
+    public boolean empty() {
+        return arr.size()==front;
+    }
+}
 
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
