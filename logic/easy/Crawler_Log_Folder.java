542bb17df8ade99435e3cc25a87536c7b266e999
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-10 22:55:17 +0530

    add the full soltion of crawler log folder which is based on java language but you can understand the thinking because logic is same

--- logic/easy/Crawler_Log_Folder.java
+++ logic/easy/Crawler_Log_Folder.java
@@ -1,3 +1,21 @@
 //https://leetcode.com/problems/crawler-log-folder/description/?envType=daily-question&envId=2024-07-10
 
 //1598. Crawler Log Folder
+
+class Solution {
+    public int minOperations(String[] logs) {
+        int directory = 0;
+        for(int i=0;i<logs.length;i++){
+            if(logs[i].equals("../")){
+                if(directory>0){
+                    directory--;
+                }
+            }else if(logs[i].equals("./")){
+                continue;
+            }else{
+                directory++;
+            }
+        }
+        return directory;
+    }
+}
