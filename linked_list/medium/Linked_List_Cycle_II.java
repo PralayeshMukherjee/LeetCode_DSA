fe283a70e2c507fb6ae4526f60935cfa4cfad2b1
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-12 21:14:54 +0530

    add the solution of the question which is based on hashmap concept

--- linked_list/medium/Linked_List_Cycle_II.java
+++ linked_list/medium/Linked_List_Cycle_II.java
@@ -1,3 +1,19 @@
 //https://leetcode.com/problems/linked-list-cycle-ii/description/
 
 // 142. Linked List Cycle II
+
+class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode temp = head;
+        HashMap<ListNode,Integer> map = new HashMap<>();
+        while(temp!=null){
+            if(map.containsKey(temp)){
+                return temp;
+            }else{
+                map.put(temp,map.getOrDefault(temp,0)+1);
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
