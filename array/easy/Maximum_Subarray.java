7f9281afdcb519d7879b28a42c8e965017ad3434
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-13 18:28:21 +0530

    add the leetcode solution of the question

--- array/easy/Maximum_Subarray.java
+++ array/easy/Maximum_Subarray.java
@@ -1,3 +1,18 @@
 //https://leetcode.com/problems/maximum-subarray/description/
 
 //53. Maximum Subarray
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int finalSum = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            finalSum = Math.max(finalSum,sum);
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return finalSum;
+    }
+}
