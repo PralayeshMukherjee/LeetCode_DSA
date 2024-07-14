0084723e3a25991986a2957652f5a1d6833cbee6
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-14 23:11:10 +0530

    add the brute force solution of the question

--- string/easy/Lexicographically_Smallest_String_After_a_Swap.java
+++ string/easy/Lexicographically_Smallest_String_After_a_Swap.java
@@ -1,1 +1,20 @@
 // Lexicographically Smallest String After a Swap
+
+class Solution {
+    public String getSmallestString(String s) {
+        char [] ch = s.toCharArray();
+        for(int i=0;i<ch.length-1;i++){
+            if(ch[i]%2==ch[i+1]%2){
+                int a = ch[i]-'0';
+                int b = ch[i+1]-'0';
+                if(a>b){
+                    char c = ch[i];
+                    ch[i] = ch[i+1];
+                    ch[i+1] = c;
+                    break; 
+                }
+            }
+        }
+        return new String(ch);
+    }
+}
