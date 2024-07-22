a2a5d2a15793bd8abffb0025320211c2e6d74e73
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-22 13:54:01 +0530

    add the helper function f which convert infix ot prefix

--- Stack/prefix_infix_postfix/Infixtoprefix.java
+++ Stack/prefix_infix_postfix/Infixtoprefix.java
@@ -1,5 +1,8 @@
 package Stack.prefix_infix_postfix;
 public class Infixtoprefix{
+    public static String f(String s){
+        
+    }
     public static void main(String[] args) {
         String s = "(A+B)*C-D+F";
         
