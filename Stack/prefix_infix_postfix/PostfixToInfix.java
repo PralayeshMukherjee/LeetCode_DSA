9f652fdbb26691e57df44cf28256e3efcd7461f0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-23 11:47:00 +0530

    add the method which convert postfix to infix

--- Stack/prefix_infix_postfix/PostfixToInfix.java
+++ Stack/prefix_infix_postfix/PostfixToInfix.java
@@ -1,8 +1,28 @@
 package Stack.prefix_infix_postfix;
 
+import java.util.Stack;
+
 public class PostfixToInfix {
+    public static String postToInfix(String exp) {
+        // code here
+        Stack<String> stk = new Stack<>();
+        int i = 0;
+        while(i<exp.length()){
+            char ch = exp.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+                stk.push(String.valueOf(ch));
+            }else{
+                String t1 = stk.pop();
+                String t2 = stk.pop();
+                String con = "("+t2+ch+t1+")";
+                stk.push(con);
+            }
+            i++;
+        }
+        return stk.peek();
+    }
     public static void main(String[] args) {
         String exp = "AB-DE+F*/";
-        
+
     }
 }
