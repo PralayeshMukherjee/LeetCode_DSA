31d519f38f5ca6bfbe5d54a5dc06fec329942720
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-07-23 12:09:44 +0530

    add the method that convert prefix to postfix

--- Stack/prefix_infix_postfix/PrefixToPostfix.java
+++ Stack/prefix_infix_postfix/PrefixToPostfix.java
@@ -1,6 +1,26 @@
 package Stack.prefix_infix_postfix;
 
+import java.util.Stack;
+
 public class PrefixToPostfix {
+    public static String preToPost(String pre_exp) {
+        // code here
+        Stack<String> st = new Stack<>();
+        int i = pre_exp.length()-1;
+        while(i>=0){
+            char ch = pre_exp.charAt(i);
+            if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+                st.push(String.valueOf(ch));
+            }else{
+                String t1 = st.pop();
+                String t2 = st.pop();
+                String con = t1+t2+ch;
+                st.push(con);
+            }
+            i--;
+        }
+        return st.peek();
+    }
     public static void main(String[] args) {
         String pre_exp = "*-A/BC-/AKL\r\n";
     }
