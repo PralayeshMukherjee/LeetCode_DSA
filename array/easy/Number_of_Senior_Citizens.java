ad99c571ec22a824c95f05ad8d6aa94379fe3baf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-01 20:08:56 +0530

    add the question solution it is very easy

--- array/easy/Number_of_Senior_Citizens.java
+++ array/easy/Number_of_Senior_Citizens.java
@@ -1,3 +1,17 @@
 // https://leetcode.com/problems/number-of-senior-citizens/description/
 
 // 2678. Number of Senior Citizens
+
+class Solution {
+    public int countSeniors(String[] details) {
+        int count = 0;
+        for(int i=0;i<details.length;i++){
+            String s = details[i].substring(11,13);
+            int age = Integer.parseInt(s);
+            if(age>60){
+                count++;
+            }
+        }
+        return count;
+    }
+}
