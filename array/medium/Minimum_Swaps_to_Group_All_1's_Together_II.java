ecaca330dec349b5b1d5b7d915ca76e980411955
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-02 20:53:32 +0530

    add the solution of the question

--- array/medium/Minimum_Swaps_to_Group_All_1's_Together_II.java
+++ array/medium/Minimum_Swaps_to_Group_All_1's_Together_II.java
@@ -1,3 +1,20 @@
 // https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/description/
 
 // 2134. Minimum Swaps to Group All 1's Together II
+
+class Solution {
+    public int minSwaps(int[] nums) {
+        int k = Arrays.stream(nums).sum();
+        int n = nums.length;
+        int cnt = 0;
+        for (int i = 0; i < k; ++i) {
+            cnt += nums[i];
+        }
+        int mx = cnt;
+        for (int i = k; i < n + k; ++i) {
+            cnt += nums[i % n] - nums[(i - k + n) % n];
+            mx = Math.max(mx, cnt);
+        }
+        return k - mx;
+    }
+}
