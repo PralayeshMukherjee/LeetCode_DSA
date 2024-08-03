bec31206ea5cb624f064d5df97cc4ad182d06874
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-03 23:03:37 +0530

    add the question number and name on leetcode

--- array/easy/Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
+++ array/easy/Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
@@ -1,1 +1,3 @@
 // https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/description/?envType=daily-question&envId=2024-08-03
+
+// 1460. Make Two Arrays Equal by Reversing Subarrays
