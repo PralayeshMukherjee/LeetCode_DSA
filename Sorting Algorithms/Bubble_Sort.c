a4742472503ae35edddf47080bbeffcdd997c036
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-04 21:02:43 +0530

    total code of bubble sort in C programming language

---
+++ Sorting Algorithms/Bubble_Sort.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+void swap(int a,int b,int *arr){
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+void bubbleSort(int *arr,int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(j,j+1,arr);
+            }
+        }
+    }
+}
+int main(){
+    printf("Enter the Size of the Array => \n");
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the element of the array => \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The old array is => \n");
+    for(int i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+
+    bubbleSort(arr,n);
+
+    printf("Print the array after sorting => \n");
+    for(int i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+
+    return 0;
+}
