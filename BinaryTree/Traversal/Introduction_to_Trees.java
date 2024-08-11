26b59fbebbbc5f64079409667be2957d7c1ec2ff
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-11 21:40:42 +0530

    write the package according to my file system

--- BinaryTree/Traversal/Introduction_to_Trees.java
+++ BinaryTree/Traversal/Introduction_to_Trees.java
@@ -1,5 +1,5 @@
 // https://www.geeksforgeeks.org/problems/introduction-to-trees/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=introduction-to-trees
-
+package BinaryTree.Traversal;
 class Solution {
     static int countNodes(int i) {
         // code here
