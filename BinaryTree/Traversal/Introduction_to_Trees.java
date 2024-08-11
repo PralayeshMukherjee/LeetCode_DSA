0efd0ad792e02309c2fcd6753d34e0322ab5c547
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-11 21:40:10 +0530

    add the solution of the question

--- BinaryTree/Traversal/Introduction_to_Trees.java
+++ BinaryTree/Traversal/Introduction_to_Trees.java
@@ -1,1 +1,8 @@
 // https://www.geeksforgeeks.org/problems/introduction-to-trees/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=introduction-to-trees
+
+class Solution {
+    static int countNodes(int i) {
+        // code here
+        return (int)Math.pow(2,i-1);
+    }
+}
