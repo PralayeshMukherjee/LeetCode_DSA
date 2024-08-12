8fe288afc5003a39d66d3b7df4d9527f1020be7b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-12 10:35:48 +0530

    add the solution of the question and import java util files

--- BinaryTree/Traversal/Postorder_Traversal.java
+++ BinaryTree/Traversal/Postorder_Traversal.java
@@ -3,6 +3,20 @@
 // 145. Binary Tree Postorder Traversal
 package BinaryTree.Traversal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Postorder_Traversal {
-    
+    public void postorderTraversalHelper(TreeNode root,List<Integer> list){
+        if(root==null) return;
+        postorderTraversalHelper(root.left,list);
+        postorderTraversalHelper(root.right,list);
+        list.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        postorderTraversalHelper(root,list);
+        return list;
+    }
 }
