433e87965e3285d614fd077281622063e1a3d07e
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-12 10:36:38 +0530

    add a TreeNode class for the referance

--- BinaryTree/Traversal/Postorder_Traversal.java
+++ BinaryTree/Traversal/Postorder_Traversal.java
@@ -5,7 +5,18 @@ package BinaryTree.Traversal;
 
 import java.util.ArrayList;
 import java.util.List;
-
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class Postorder_Traversal {
     public void postorderTraversalHelper(TreeNode root,List<Integer> list){
         if(root==null) return;
