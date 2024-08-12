77cce2cea75efee38946a3f4083243ab7495e88f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-12 10:24:32 +0530

    add the solution of the leetcode problem and import the java util files for ArrayList, list

--- BinaryTree/Traversal/Inorder_Traversal.java
+++ BinaryTree/Traversal/Inorder_Traversal.java
@@ -2,6 +2,23 @@
 
 // 94. Binary Tree Inorder Traversal
 package BinaryTree.Traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
 public class Inorder_Traversal {
-    
+    public void inorderTraversalHelper(TreeNode root,List<Integer> list){
+        if(root==null) return;
+        inorderTraversalHelper(root.left,list);
+        list.add(root.val);
+        inorderTraversalHelper(root.right,list);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        inorderTraversalHelper(root,list);
+        return list;
+    }
 }
