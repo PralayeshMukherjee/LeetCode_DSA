9d3b5a60a545666140b4567148bc3d0c5b99e2bb
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-12 10:30:49 +0530

    add the leetcode solution of the question

--- BinaryTree/Traversal/Preorder_Traversal.java
+++ BinaryTree/Traversal/Preorder_Traversal.java
@@ -4,6 +4,20 @@
 
 package BinaryTree.Traversal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Preorder_Traversal {
-    
+    public void preorderTraversalHelper(TreeNode root,List<Integer> list){
+        if(root==null) return;
+        list.add(root.val);
+        preorderTraversalHelper(root.left,list);
+        preorderTraversalHelper(root.right,list);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null) return list;
+        preorderTraversalHelper(root,list);
+        return list;
+    }
 }
