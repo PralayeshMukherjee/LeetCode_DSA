2c30aa0aa673abfd1a12cb255d29bb4272ea7acc
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-14 22:59:10 +0530

    add the leetcode solution of the question

--- BinaryTree/Traversal/Maximum_Depth_of_Binary_Tree.java
+++ BinaryTree/Traversal/Maximum_Depth_of_Binary_Tree.java
@@ -1,5 +1,10 @@
 package BinaryTree.Traversal;
 
 public class Maximum_Depth_of_Binary_Tree {
-    
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int a = maxDepth(root.left);
+        int b = maxDepth(root.right);
+        return Math.max(a,b)+1;
+    }
 }
