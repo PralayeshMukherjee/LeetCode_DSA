cb1196d4c661119192b95f9db7bbd1c0d76b9477
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-16 22:49:21 +0530

    add the solution of the question

--- BinaryTree/Traversal/Binary_Tree_Zigzag_Level_Order_Traversal.java
+++ BinaryTree/Traversal/Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -3,6 +3,37 @@
 // 103. Binary Tree Zigzag Level Order Traversal
 package BinaryTree.Traversal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Binary_Tree_Zigzag_Level_Order_Traversal {
-    
+    public int flag = 1;
+    public void zig(TreeNode root,List<List<Integer>> list,Queue<TreeNode> queue){
+        if(root==null) return;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> ll = new ArrayList<>();
+            for(int i=0;i<levelNum;i++){
+                TreeNode node = queue.poll();
+                if(node.left!=null) queue.add(node.left);
+                if(node.right!=null) queue.add(node.right);
+                ll.add(node.val);
+            }
+            if(flag==-1){
+                Collections.reverse(ll);
+            }
+            flag *= -1;
+            list.add(new ArrayList<>(ll));
+        }
+    }
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        zig(root,list,queue);
+        return list;
+    }
 }
