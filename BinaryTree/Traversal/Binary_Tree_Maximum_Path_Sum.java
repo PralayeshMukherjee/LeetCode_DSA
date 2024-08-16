97f2c70a4db5bf0fb475da4d7fe0d69e917173ef
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-08-16 20:56:31 +0530

    add the solution of the question

--- BinaryTree/Traversal/Binary_Tree_Maximum_Path_Sum.java
+++ BinaryTree/Traversal/Binary_Tree_Maximum_Path_Sum.java
@@ -4,5 +4,17 @@
 package BinaryTree.Traversal;
 
 public class Binary_Tree_Maximum_Path_Sum {
-    
+    public int max;
+    public int maxi(TreeNode node){
+        if(node==null) return 0;
+        int leftNode = Math.max(0,maxi(node.left));
+        int rightNode = Math.max(0,maxi(node.right));
+        max = Math.max(max,node.val+leftNode+rightNode);
+        return node.val+Math.max(leftNode,rightNode);
+    }
+    public int maxPathSum(TreeNode root) {
+        max = Integer.MIN_VALUE;
+        maxi(root);
+        return max;
+    }
 }
